6c2d507dfe004b41cf83dca01ff7d86a71995636
brian.chung <brian.chung@aylanetworks.com>
2019-04-15 11:02:28 +0800

    brian update all custom config

--- pre-install.sh
+++ pre-install.sh
@@ -6,5 +6,14 @@ if [ "$USER" != "root" ];then
     sed -i 's/root/ubuntu/g' KubeAutomation/template/hosts.ini.j2
 fi
 
-apt-get update
-apt-get install -y docker ansible
+read -p 'which cloud provider you use? (gcp/aws)' CLOUD
+
+if [ "$CLOUD" = "gcp" ];then
+  sudo apt-get update
+  sudo apt-get install -y docker.io ansible
+else
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update
+  sudo apt-get install -y docker-ce
+fi
